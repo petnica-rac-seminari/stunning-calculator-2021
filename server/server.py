f34910b77d056b3f0d14b6155d738a016b32444f
= <mikmikyt1011@gmail.com>
2021-10-24 18:53:53 +0200

    sada salje samo list, ubacena provera duzine, 6969 port zbog mog port forwarda

--- server/server.py
+++ server/server.py
@@ -9,7 +9,9 @@ app = Flask(__name__)
 @app.route('/recognise_image', methods=['POST'])
 def handlePOSTReq():
     try:
-        image = ImageObject.parse_obj(request.json)
+        image = ImageObject.parse_obj(request.json).image
+        if (len(image) != 784):
+            raise Exception("nema dovoljno")
         try:
             number = MLTest(image)
             return jsonify(number), 200
@@ -19,4 +21,4 @@ def handlePOSTReq():
         return jsonify("Invalid format!"), 400
     
     
-app.run(host = '0.0.0.0' )
+app.run(host = '0.0.0.0', port=6969 )
