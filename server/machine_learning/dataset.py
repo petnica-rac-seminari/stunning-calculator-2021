3c511f97187114c8a46b4287fe2c498f8b48c016
Dario Vajda <vajdadario@gmail.com>
2021-10-24 16:48:48 +0200

    Formatiranje mnist dataseta

--- server/machine_learning/dataset.py
+++ server/machine_learning/dataset.py
@@ -1,8 +1,30 @@
-from keras.datasets import mnist
+from mlxtend.data import loadlocal_mnist
+import numpy as np
 
-(X, y), (Xt, yt) = mnist.load_data() #training and testing data
+def normalize(x: np.ndarray) -> np.ndarray:
+    return (x - x.min()) / (x.max() - x.min())
 
-print('X_train: ' + str(X.shape))
-print('Y_train: ' + str(y.shape))
-print('X_test:  '  + str(Xt.shape))
-print('Y_test:  '  + str(yt.shape))
+def vectorize(y: np.ndarray) -> np.ndarray:
+    vector_y = np.zeros((y.shape[0], 10))
+    for i in range (0, y.shape[0]):
+        vector_y[i, y[i]] = 1
+    print(vector_y[0])
+    print(y[0])
+    return vector_y
+
+def get_data():
+    X, y = loadlocal_mnist(
+        "train-images.idx3-ubyte",
+        "train-labels.idx1-ubyte"
+    )
+    Xt, yt = loadlocal_mnist(
+        "t10k-images.idx3-ubyte",
+        "t10k-labels.idx1-ubyte"
+    )
+    X = normalize(X)
+
+    y = vectorize(y)
+    yt = vectorize(yt)
+    return X, y, Xt, yt
+
+get_data()
--- server/machine_learning/main.py
+++ server/machine_learning/main.py
@@ -1,4 +1,22 @@
+import numpy as np
 
-class Model():
-    def predict_number(X):
-        return 0
+import dataset as data
+
+X, y, Xt, yt = data.get_data()
+
+#region init w and b
+w1 = np.random.uniform(-10, 10, (X.shape[1], 400)) * 1e-1
+b1 = np.random.uniform(-10, 10, (1, 400)) * 1e-1
+
+w2 = np.random.uniform(-10, 10, (400 // 2, 100)) * 1e-1
+b2 = np.random.uniform(-10, 10, (1, 100)) * 1e-1
+
+w3 = np.random.uniform(-10, 10, (100, 10)) * 1e-1
+b3 = np.random.uniform(-10, 10, (1, 10)) * 1e-1
+#endregion
+
+#region hyperparams
+epoch = int(1e3)
+lr = 1e-3
+
+#endregion
