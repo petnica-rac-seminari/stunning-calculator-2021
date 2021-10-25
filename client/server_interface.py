0b8e37ce3609d3518a29cc4cade551b504a1585b
Marko Radojevic <marko.d.radojevic@gmail.com>
2021-10-25 11:51:48 +0200

    preparing merge

--- client/server_interface.py
+++ client/server_interface.py
@@ -3,11 +3,12 @@ import numpy
 import json
 
 def SendParsedImage(arr : list) -> str:
-    try:                    
-        print(arr)
-        value = requests.post('http://localhost:5000/recognise_image', json = { 
-            'image' : arr
-            })                        
+    try:
+        return '42'                    
+        #print(arr)
+        #value = requests.post('http://localhost:5000/recognise_image', json = { 
+        #    'image' : arr
+        #    })                        
         return str(value.json())         
     except:        
         raise Exception("lele2")               
